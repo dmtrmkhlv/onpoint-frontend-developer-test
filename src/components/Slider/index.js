5313367bca66c6ea1adfd7bdb8cc437225740e12
Dmitry Mikhailov <app@dmtrmkhlv.ru>
2022-02-11 14:45:23 +0300

    swipe anim

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -1,14 +1,30 @@
-import {React, useState} from "react";
+import {React, useState, useEffect} from "react";
 import styles from "../../styles/App.module.css";
 
 export const Slider = (props) => {
   const [startCoordinates, setStartCoordinates] = useState(0);
   const [endCoordinates, setEndCoordinates] = useState(0);
   const [prevEndCoordinates, setPrevEndCoordinates] = useState(0);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const [moveStatus, setMoveStatus] = useState(false);
   const divStyle = {
     left: `-${endCoordinates}px`
   };
+
+  const swipeAnimation = (x, width)=>{
+    let newCoordinates = 0;
+    if(x < width/2){
+
+    }
+    if(x >= width/2){
+      newCoordinates = width;
+    }
+    if(x > width && x <= (width + width/2)){
+      newCoordinates = width * 2;
+    }
+    setEndCoordinates(newCoordinates);
+    setPrevEndCoordinates(newCoordinates);
+  }
 
   const isTouch = () => 'ontouchstart' in window || (window.DocumentTouch && document instanceof window.DocumentTouch) || navigator.maxTouchPoints > 0 || window.navigator.msMaxTouchPoints > 0;
       
@@ -19,6 +35,9 @@ export const Slider = (props) => {
     // To left
     if(startCoordinates > xCoordinates){
       newEndCoordinates = Math.round(prevEndCoordinates + (startCoordinates - xCoordinates));
+      if(newEndCoordinates >= windowWidth * 2){
+        return;
+      }
       setEndCoordinates(newEndCoordinates);
     }
     // To right
@@ -36,6 +55,7 @@ export const Slider = (props) => {
   const endSwipe = ()=>{
     setMoveStatus(false);
     setPrevEndCoordinates(endCoordinates);
+    swipeAnimation(endCoordinates, windowWidth);
   }
   
   const swipe = (e)=>{
